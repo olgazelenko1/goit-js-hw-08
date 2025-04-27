1e044955fa31a41c3497cf8797af042d08e60e1a
OLGA ZELENKO <olichkazelenko@gmail.com>
2025-04-27 17:57:48 +0100

    fixbag

--- js/gallery.js
+++ js/gallery.js
@@ -20,6 +20,48 @@ const images = [
       'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
     description: 'Aerial Beach View',
   },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg',
+    description: 'Flower Blooms',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+    description: 'Alpine Mountains',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg',
+    description: 'Mountain Lake Sailing',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg',
+    description: 'Alpine Spring Meadows',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg',
+    description: 'Nature Landscape',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
+    description: 'Lighthouse Coast Sea',
+  },
 ];
 
 const container = document.querySelector('.gallery');
@@ -84,14 +126,12 @@ function handleCloseModal(e) {
 container.addEventListener('click', (e) => {
   e.preventDefault();
 
-  const liElem = e.target.closest('li');
-  if (!liElem) return;
+  if (e.target.nodeName !== 'IMG') return;
 
-  const id = liElem.dataset.id;
-  const image = imagesCopy.find((el) => el.id == id);
-  if (image) {
-    openModal(image);
-  }
+  const original = e.target.dataset.source;
+  const description = e.target.alt;
+
+  openModal({ original, description });
 });
 
 renderImages();
